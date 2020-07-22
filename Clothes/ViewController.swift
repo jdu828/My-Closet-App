e6131b38d8cce717bf006177966bcc6c5d15f814
Jeffrey Du <jeffreydu828@gmail.com>
2020-07-21 20:58:05 -0400

    Initial Commit

---
+++ Clothes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Clothes
+//
+//  Created by Jeffrey Du on 7/21/20.
+//  Copyright © 2020 Jeffrey Du. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
